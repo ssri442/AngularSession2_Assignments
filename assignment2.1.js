9553254737488437a0de3db76925cc0a1db22bfb
Sai <33226057+ssri442@users.noreply.github.com>
2017-12-09 19:03:54 +0530

    Add files via upload

---
+++ assignment2.1.js
@@ -0,0 +1,5 @@
+var Svalue = function (name, rollNo) {
+    return "Dispaly Message is " + name + rollNo;
+};
+var myValue = Svalue("hello", 9);
+console.log(myValue);
---
+++ assignment2.1.ts
@@ -0,0 +1,5 @@
+let Svalue = (name : string , rollNo : number) : string => { 
+    return "Dispaly Message is " + name + rollNo;
+    }
+    let myValue = Svalue("hello" , 9);
+    console.log(myValue);
